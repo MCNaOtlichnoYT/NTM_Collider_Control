74434018d79c7cc7409775a000ccf12e0b5d4b38
MCNaOtlichnoYT <105880094+MCNaOtlichnoYT@users.noreply.github.com>
2025-06-26 20:02:01 +0300

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,3 +1,6 @@
+os.execute('rm hadron.lua -f')
+os.execute('rm hadronconfig.lua -f')
+os.execute()
 os.execute('wget https://raw.githubusercontent.com/MCNaOtlichnoYT/NTM_Collider_Control/refs/heads/main/hadron.lua hadron.lua')
 os.execute('wget https://raw.githubusercontent.com/MCNaOtlichnoYT/NTM_Collider_Control/refs/heads/main/hadronconfig.lua hadronconfig.lua')
 os.execute('rm installer.lua')
